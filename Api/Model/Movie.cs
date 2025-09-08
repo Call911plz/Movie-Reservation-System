e96b55a1534a7335a0ccda446b0d530d799f0749
Hieu Truong <htruong6219@gmail.com>
2025-09-07 21:07:38 -0700

    Add movie class

---
+++ Api/Model/Movie.cs
@@ -0,0 +1,14 @@
+public class Movie
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImagePath { get; set; } = string.Empty;
+    public float Cost { get; set; }
+    public TimeOnly PlayLength { get; set; }
+    public string Genre { get; set; } = string.Empty;
+    public DateTime AnnouncmentTime { get; set; }
+    public DateTime ShowTime { get; set; }
+    public required List<List<Seat>> Seats { get; set; }
+    public int AvailableSeatsCount { get; set; }
+}
