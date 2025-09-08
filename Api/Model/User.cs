05b22025021c923849e897ff994e86e72de400fa
Hieu Truong <htruong6219@gmail.com>
2025-09-07 21:31:44 -0700

    Add username password field to user class

--- Api/Model/User.cs
+++ Api/Model/User.cs
@@ -5,4 +5,6 @@ public class User
     public string Name { get; set; } = string.Empty;
     public string? ReservedSeat { get; set; } = null;
     public bool IsAdmin { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
