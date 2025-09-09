3f7f188a31900924528c8bfa85b85906f50f5584
Hieu Truong <htruong6219@gmail.com>
2025-09-08 23:32:32 -0700

    Add movie management service

---
+++ Api/Service/ManageMovieService.cs
@@ -0,0 +1,32 @@
+public interface IManageMovieService
+{
+    public Task<Movie?> CreateMovieAsync(Movie newMovie);
+    public List<Movie> GetMovies();
+    public Task<Movie?> UpdateMovieAsync(Movie movieToUpdate);
+    public Task<bool?> DeleteMovieAsync(Movie movieToDelete);
+}
+
+public class ManageMovieService(IMovieRepository repo) : IManageMovieService
+{
+    IMovieRepository _repo = repo;
+
+    public async Task<Movie?> CreateMovieAsync(Movie newMovie)
+    {
+        return await _repo.CreateMovieAsync(newMovie);
+    }
+
+    public async Task<bool?> DeleteMovieAsync(Movie movieToDelete)
+    {
+        return await _repo.DeleteMovieAsync(movieToDelete);
+    }
+
+    public List<Movie> GetMovies()
+    {
+        return _repo.GetMovies();
+    }
+
+    public Task<Movie?> UpdateMovieAsync(Movie movieToUpdate)
+    {
+        return _repo.UpdateMovieAsync(movieToUpdate);
+    }
+}
