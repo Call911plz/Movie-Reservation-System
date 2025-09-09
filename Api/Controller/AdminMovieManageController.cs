b9812e26a67175984ffa4786ab7b0e8bb21f0543
Hieu Truong <htruong6219@gmail.com>
2025-09-09 00:15:30 -0700

    Add movie management controller

---
+++ Api/Controller/AdminMovieManageController.cs
@@ -0,0 +1,38 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class AdminMovieManageController(IManageMovieService service) : ControllerBase
+{
+    protected readonly IManageMovieService _service = service;
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Movie>> CreateMovieAsync(Movie newMovie)
+    {
+        return Ok(await _service.CreateMovieAsync(newMovie));
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Admin")]
+    public ActionResult<List<Movie>> GetMovies()
+    {
+        return Ok(_service.GetMovies());
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<Movie>> UpdateMovieAsync(Movie movieToUpdate)
+    {
+        return Ok(await _service.UpdateMovieAsync(movieToUpdate));
+    }
+
+    [HttpDelete]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult<bool>> DeleteMovieAsync(Movie movieToDelete)
+    {
+        return Ok(await _service.DeleteMovieAsync(movieToDelete));
+    }
+}
